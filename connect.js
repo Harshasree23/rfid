92e83956f5bb0ba5c90206970af9a06b68649ee5
Harshasree23 <sreeharsha2358@gmail.com>
2025-03-24 13:39:01 +0530

    Changed mongodb connection name

--- connect.js
+++ connect.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const makeConnection = async ( name) => {
-    return await mongoose.connect(`mongodb+srv://marvelavengersharsha:${process.env.db_password}@cluster0.xv2jk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/${name}`);
+const makeConnection = async (name) => {
+    return await mongoose.connect(`mongodb+srv://marvelavengersharsha:${process.env.db_password}@cluster0.xv2jk.mongodb.net/${name}?retryWrites=true&w=majority&appName=Cluster0`);
 }
 
 module.exports = {
     makeConnection,
-}
+};
